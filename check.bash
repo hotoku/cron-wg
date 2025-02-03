a73a778388df85c7976abb5c9aa4a519e1cb1283
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2025-02-03 21:03:53 +0900

    [upd] send to slack every 10 minutes.

--- check.bash
+++ check.bash
@@ -1,44 +1,53 @@
 #!/bin/bash
 
+
 export PATH=/opt/homebrew/bin:"$PATH"
+
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 LOG_FILE="${SCRIPT_DIR}"/cron.log
 LOGS_URL=$(cat "${SCRIPT_DIR}"/credentials/urls.json |jq -r '.logs')
-WARNS_URL=$(cat "${SCRIPT_DIR}"/credentials/urls.json |jq -r '.warnings')
+REPORT_FILE="${SCRIPT_DIR}"/report.txt
 
 
 send_message(){
     local msg=$1
-    local dest=$2
-    if [[ "${dest}" = "logs" ]]
-    then
-        webhook_url="${LOGS_URL}"
-    elif [[ "${dest}" = "warns" ]]
-    then
-        webhook_url="${WARNS_URL}"
-    else
-        msg="error: invalid destination. original message: ${msg}"
-        webhook_url=${LOGS_URL}
-    fi
-    echo "sending to ${dest} with message: ${msg}" >> "${LOG_FILE}"
-    echo "webhook url: ${webhook_url}" >> "${LOG_FILE}"
-    local tmp='{"text": "cron-wg: __MESSAGE__"}'
-    local payload=$(echo ${tmp} | sed -e "s/__MESSAGE__/${msg}/")    
-    curl -X POST -H "Content-type: application/json" -d "${payload}" "${webhook_url}" >> "${LOG_FILE}" 2>&1
+
+
+    echo "sending a message to slack" >> "${LOG_FILE}"
+    echo "message: ${msg}" >> "${LOG_FILE}"
+    echo "webhook url: ${LOGS_URL}" >> "${LOG_FILE}"
+
+
+    local msg_escaped=$(echo "${msg}" | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g')
+    local template='{"text": "__MESSAGE__"}'
+    local payload=$(echo "${template}" | sed -e "s/__MESSAGE__/${msg_escaped}/")
+
+
+    curl -X POST -H "Content-type: application/json" -d "${payload}" "${LOGS_URL}" >> "${LOG_FILE}" 2>&1
     echo "" >> "${LOG_FILE}" # curlが末尾の改行を出力しないので追加
 }
 
+
 date +"%Y-%m-%d %H:%M:%S start checking" >> "${LOG_FILE}"
+
 
 if ! [[ -f /var/run/wireguard/wg0.name ]]; then
     echo "Wireguard is down" >> "${LOG_FILE}"
-    # send_message "Wireguard is down" "warns"
+    date "+%y-%m-%d %H:%M:%S down" >> "${REPORT_FILE}"
     wg-quick down wg0 >> "${LOG_FILE}" 2>&1
     wg-quick up wg0 >> "${LOG_FILE}" 2>&1
 else
     echo "Wireguard is already up" >> "${LOG_FILE}"
-    # send_message "Wireguard is already up" "logs"
+    date "+%y-%m-%d %H:%M:%S up" >> "${REPORT_FILE}"
 fi
 
+
+chown hotoku:staff "${REPORT_FILE}"
 chown hotoku:staff "${LOG_FILE}"
+
+
+if [[ $(cat "${REPORT_FILE}" | wc -l) -gt 10 ]]; then
+    send_message "$(cat "${REPORT_FILE}")" logs
+    rm "${REPORT_FILE}"
+fi
